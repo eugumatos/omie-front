e39a72dd382445dae68596215c9cd3b1492fa0da
gustavo-matos <guamatos@outlook.com>
2022-07-18 23:34:12 -0300

    finished app

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,11 @@
 import styles from './Search.module.css';
 
-export function Search() {
+export function Search({ children }) {
   return(
     <article className={styles.search}>
       <header>
-        <form className={styles.searchForm}>
-          <strong> Busque por um usuário </strong>
-
-          <textarea 
-            name="search"
-            placeholder='Busque por um usuário' 
-            required
-          />
-          
-          <footer>
-            <button type='submit' >
-              Publicar
-            </button>
-          </footer>
-        </form>
+        { children }
       </header>
-
-  
     </article>
   )
 }
